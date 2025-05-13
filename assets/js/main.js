a77ab909331569677ba9a2dfedafbd7f16ca56af
huynhduy <nlhd.2701@gmail.com>
2025-05-13 10:57:47 +0900

    done skils 59:05

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,4 +27,29 @@ function linkAction(){
     navMenu.classList.remove('show-menu');
     console.log(this.textContent);
 };
-navLink.forEach(n => n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click',linkAction));
+
+// Accordion skills
+// để tạo hiệu ứng accordion cho phần "Skills" – tức là người dùng click vào tiêu đề kỹ năng thì nội dung mở ra, click vào cái khác thì phần trước đóng lại.
+const skillsContent = document.getElementsByClassName('skills__content'),
+        skillsHeader = document.querySelectorAll('.skills__header');
+function toggleSkills(){
+    let itemClass = this.parentNode.className;
+    // this.parentNode: là .skills__content (phần tử cha chứa cả tiêu đề và nội dung kỹ năng).
+    // lấy class hiện tại của phần tử cha đó.
+
+    for(i=0; i<skillsContent.length;i++){
+        skillsContent[i].className= 'skills__content skills__close';
+        // Trước khi mở cái nào đó, đoạn này đóng toàn bộ các skills đang mở, bằng cách gán lại class skills__close.
+
+
+    }
+    if (itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open';
+        // Nếu phần đó đang đóng, thì đổi class thành skills__open để mở ra.
+        
+    }
+}
+skillsHeader.forEach((el)=>{
+    el.addEventListener('click',toggleSkills);
+})
